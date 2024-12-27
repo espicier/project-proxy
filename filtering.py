590ff90ca279a02362b2ee90b1efe7471f5fbb0d
Corentin Clerc <corentin.clerc74@hotmail.fr>
2024-12-27 20:25:31 +0100

    Init

---
+++ filtering.py
@@ -0,0 +1,17 @@
+# Retourne le nom du serveur, le port et le document à partir de l'url
+def split_url(url):
+    return ('', '','')
+
+# Prend une réponse avec une balise html title, et insert du texte après la première balise trouvée.
+def add_text_to_title(message, text):
+    # on avait un exemple dans un des TPs
+    return message
+
+# Filtre le contenu html, en remplaçant les mots trouvés dans le fichier de configuration 
+# par des étoiles (par exemple, je sais pas ce qui est attendu par "filtrer")
+def filter_content(message):
+    return message
+
+# Enlève les lignes problématiques trouvées dans le message
+def remove_problematic_lines(message):
+    return message
---
+++ proxy.py
@@ -0,0 +1,9 @@
+import os, socket, sys
+
+import filtering as flt
+
+# à déterminer : comment on gère deux sockets
+# Socket côté client:
+
+
+# Socket côté serveur:
